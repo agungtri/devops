400bfdc0e0589d64b1e577c6c546c346fada56bb
agung <agungdrosha.com>
2022-03-30 17:14:37 +0700

    chap 24

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Models;
+using Shopping.Client.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ProductContext _context;
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ProductContext context, ILogger<ProductController> logger)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Product 
+            {
+                Name = "Agung" 
+            }).ToArray();
+        }
+
+    }
+}
